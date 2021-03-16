b724e215b8ff899b408a17d03c5cd03e412757fc
190103371 <190103371@stu.sdu.edu.kz>
2021-03-16 13:00:22 +0600

    Lab Changes

--- Desktop/xampp/htdocs/PHPinVSC/lab_Profile/tests/Feature/ProfileTest.php
+++ Desktop/xampp/htdocs/PHPinVSC/lab_Profile/tests/Feature/ProfileTest.php
@@ -5,38 +5,31 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class ProfileTest extends TestCase
 {
+    use WithoutMiddleware;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    /*public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }*/
-
+  
 
     public function test_get_post_id(){
-        $response =  $this->get('/post/5');
+        $response =  $this->get('/post/13');
         $response -> assertStatus(200);
     }
     
     public function test_get_post_response(){
-        $response1 =  $this->get('/post/5');
+        $response1 =  $this->get('/post/13');
         $response1 -> assertViewHas('post');
     }
-    /*public function test_asserting_an_exact_json_match(){
-        $response2 = $this->json('POST', '/post', ['title' => 'About']);
-
-        $response2 -> assertLocation([
-            'created' => true,
-        ]);
-    }*/
+    public function test_directory(){
+        $this-> assertDirectoryDoesNotExist('C:/Users/User/Desktop/xampp/htdocs
+        /PHPinVSC/lab_Profile/resources/views/Blog');
+    }
     
 
 
